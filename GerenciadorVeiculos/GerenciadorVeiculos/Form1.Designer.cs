5e8b2f5ebf6b4c1f2f92df5693bfb54a1b665a0f
LUÍSA LIMA DE MARQUES OLIVEIRA <luisa.oliveira@e239.local>
2026-09-23 15:02:01 -0300

    Entrega da Prova Pratica de DS - Cadastro de Veiculos

---
+++ GerenciadorVeiculos/GerenciadorVeiculos/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace GerenciadorVeiculos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.txtPrecoBase = new System.Windows.Forms.TextBox();
+            this.txtParametro = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.lblParametro = new System.Windows.Forms.Label();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(37, 59);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(198, 20);
+            this.txtModelo.TabIndex = 0;
+            this.txtModelo.Text = "Campo para digitar o modelo do veículo";
+            this.txtModelo.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtPrecoBase
+            // 
+            this.txtPrecoBase.Location = new System.Drawing.Point(37, 110);
+            this.txtPrecoBase.Name = "txtPrecoBase";
+            this.txtPrecoBase.Size = new System.Drawing.Size(164, 20);
+            this.txtPrecoBase.TabIndex = 1;
+            this.txtPrecoBase.Text = "Campo para digitar o preço base";
+            // 
+            // txtParametro
+            // 
+            this.txtParametro.Location = new System.Drawing.Point(37, 214);
+            this.txtParametro.Name = "txtParametro";
+            this.txtParametro.Size = new System.Drawing.Size(164, 20);
+            this.txtParametro.TabIndex = 2;
+            this.txtParametro.Text = "Campo para portas ou cilindradas";
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Items.AddRange(new object[] {
+            "Moto",
+            "Carro"});
+            this.cmbTipo.Location = new System.Drawing.Point(37, 166);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(121, 21);
+            this.cmbTipo.TabIndex = 3;
+            // 
+            // lblParametro
+            // 
+            this.lblParametro.AutoSize = true;
+            this.lblParametro.Location = new System.Drawing.Point(34, 264);
+            this.lblParametro.Name = "lblParametro";
+            this.lblParametro.Size = new System.Drawing.Size(212, 13);
+            this.lblParametro.TabIndex = 4;
+            this.lblParametro.Text = "Rótulo que mudará de texto dinamicamente";
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(37, 298);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(105, 23);
+            this.btnSalvar.TabIndex = 5;
+            this.btnSalvar.Text = "Salvar Veículo";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.lblParametro);
+            this.Controls.Add(this.cmbTipo);
+            this.Controls.Add(this.txtParametro);
+            this.Controls.Add(this.txtPrecoBase);
+            this.Controls.Add(this.txtModelo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.TextBox txtPrecoBase;
+        private System.Windows.Forms.TextBox txtParametro;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.Label lblParametro;
+        private System.Windows.Forms.Button btnSalvar;
+    }
+}
+
